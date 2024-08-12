a007dc46d10cd01086f2dee2c88ead29559fa96c
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-08-12 00:34:30 -0500

    Initial Commit

---
+++ hello world.cs
@@ -0,0 +1,10 @@
+using system
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ hello world.ino
@@ -0,0 +1,3 @@
+void setup() {
+    Serial.print("Hello world!")
+}
